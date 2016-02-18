de313da8530a6d27b718f789b66a33945f45491a
liboz <ringil.sword@gmail.com>
2016-02-17 19:43:57 -0500

    Std Dev

--- Plot.py
+++ Plot.py
@@ -12,7 +12,7 @@ import numpy as np
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 
-np.random.seed(seed=84536895)
+np.random.seed(seed=756)
 
 regionDict = {}
 dateDict = {}
@@ -32,20 +32,27 @@ all_regions = ["americas", "europe", "se_asia", "china"]
 
 region_averages = {i : [] for i in all_regions}
 region_min = {i : [] for i in all_regions}
+region_std_dev = {i : [] for i in all_regions}
+
 
 for region, data in regionDict.items():
     for date, player_data in data.items():
         np_data = np.array([player['mmr'] for player in player_data ])
         average = np_data.mean()
         min_value = np_data.min()
+        st_dev = np_data.std()
+
         datetimeDate = parse(date)
         region_avg_data = (datetimeDate, average)
         region_averages[region].append(region_avg_data)
         
         region_min_data = (datetimeDate, min_value)
         region_min[region].append(region_min_data)
+        
+        region_std_dev_data = (datetimeDate, st_dev)
+        region_std_dev[region].append(region_std_dev_data)
 
-colors = cm.rainbow(np.linspace(0, 1, 8))
+colors = cm.rainbow(np.linspace(0, 1, 12))
 np.random.shuffle(colors)
 plt.figure(figsize=(15, 15)) # This increases resolution
 
@@ -57,7 +64,18 @@ for index, region in enumerate(all_regions):
     x_val = [x[0] for x in region_min[region]]
     y_val = [x[1] for x in region_min[region]]
     plt.scatter(x_val, y_val, label=(region+"min"), color=colors[index + 4], marker='x', s=1.25)
+    
+    
 
+plt.legend()
+
+plt.show()
+
+plt.figure(figsize=(15, 15)) # This increases resolution
+for index, region in enumerate(all_regions):
+    x_val = [x[0] for x in region_std_dev[region]]
+    y_val = [x[1] for x in region_std_dev[region]]
+    plt.scatter(x_val, y_val, label=(region+"std_dev"), color=colors[index + 8])
 
 plt.legend()
 
